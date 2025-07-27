2cf70f948d670ac798004b2822e4c706990f06be
bechir-fahmi <95341647+bechir-fahmi@users.noreply.github.com>
2025-07-27 14:34:05 +0100

    chore: Update environment configuration and dependencies
    - Updated requirements.txt to include PyMuPDF version 1.26.0.
    - Modified pyvenv.cfg and activate scripts to reflect the new virtual environment path.
    - Removed obsolete executable files and updated existing ones in the Scripts directory.

--- ai_model_env/Scripts/activate.bat
+++ ai_model_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PC\Desktop\ai model\ai_model_env
+set VIRTUAL_ENV=C:\Users\PC\Desktop\PFE 2024-2025\ai model\ai_model_env
 
 if not defined PROMPT set PROMPT=$P$G
 
